682bb1c9e3d0ce89c4564bdf671c97ad2351498e
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-08-13 11:41:01 +0530

    Add file

---
+++ advanced-taxonomy-image-banner.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Advanced Taxonomy Image and Banner images
+ * Plugin URI: https://github.com/manpreetdev21/advanced-taxonomy-image-banner.git
+ * Description: Advanced Taxonomy Image and Banner images option for the wordpress.
+ * Version: 1.0.0
+ * Author: Manpreet Singh
+ * Author URI: https://github.com/manpreetdev21/advanced-taxonomy-image-banner.git
+ * Text Domain: advanced-taxonomy-image-banner
+ * Domain Path: /languages
+ * Requires at least: 5.6
+ * Requires PHP: 7.4
+ */
+
+defined('ABSPATH') || exit;
+
+define('ATIB_PLUGIN_FILE', __FILE__);
+define('ATIB_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('ATIB_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('ATIB_VERSION', '1.0.0');
+
