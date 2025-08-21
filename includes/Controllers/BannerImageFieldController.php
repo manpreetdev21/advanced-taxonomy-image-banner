3f5a215384472900748577e7f72055a566f77708
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-08-21 18:00:23 +0530

    Add script

--- includes/Controllers/BannerImageFieldController.php
+++ includes/Controllers/BannerImageFieldController.php
@@ -7,16 +7,14 @@ defined('ABSPATH') || exit;
 class BannerImageFieldController{
 
     public $term_id = '';
-    public $banner_sliders = '';
+    public $banner_sliders = array();
 
     function __construct(){
         foreach ( $this->get_all_taxnomy_banner_slug() as $taxonomy) {
-            if( ( $taxonomy != 'nav_menu' ) && ( $taxonomy != 'post_format' ) && ( $taxonomy != 'wp_theme' ) && ( $taxonomy != 'wp_template_part_area' ) && ( $taxonomy != 'wp_pattern_category' ) ){
-                add_action( "{$taxonomy}_add_form_fields", array( $this, 'atib_add_taxonomy_banner_image_field' ) );
-                add_action( "{$taxonomy}_edit_form_fields", array( $this, 'atib_edit_taxonomy_banner_image_field' ), 10, 2 );
-                add_action( "created_{$taxonomy}", array( $this, 'atib_save_taxonomy_banner_image' ) );
-                add_action( "edited_{$taxonomy}", array( $this, 'atib_save_taxonomy_banner_image' ) );
-            }
+            add_action( "{$taxonomy}_add_form_fields", array( $this, 'atib_add_taxonomy_banner_image_field' ) );
+            add_action( "{$taxonomy}_edit_form_fields", array( $this, 'atib_edit_taxonomy_banner_image_field' ), 10, 2 );
+            add_action( "created_{$taxonomy}", array( $this, 'atib_save_taxonomy_banner_image' ) );
+            add_action( "edited_{$taxonomy}", array( $this, 'atib_save_taxonomy_banner_image' ) );
         }
     }
 
@@ -26,25 +24,47 @@ class BannerImageFieldController{
 
     public function atib_edit_taxonomy_banner_image_field($term, $taxonomy) {
         $this->term_id = $term->term_id;
-        $this->banner_sliders = get_term_meta($term_id, 'taxonomy-banner-image-id', true);
-        $this->banner_sliders = !empty($banner_sliders) ? $banner_sliders : array('');
+        $this->banner_sliders = get_term_meta( $this->term_id, 'taxonomy-banner-image-id', true);
+        
+        // Ensure we always have an array, even if empty
+        if (empty($this->banner_sliders) || !is_array($this->banner_sliders)) {
+            $this->banner_sliders = array('');
+        }
+        
         include ADVANCED_TAXONOMY_IMAGES_PLUGIN_PATH . 'includes/Views/Admin/edit_taxonomy_banner_image.php';
     }
 
     public function atib_save_taxonomy_banner_image($term_id) {
-        if (isset($_POST['taxonomy-banner-image-id'])) {
-            delete_term_meta($term_id, 'taxonomy-banner-image-id');            
-            foreach ($_POST['taxonomy-banner-image-id'] as $banner_slider) {
-                if (!empty($banner_slider)) {
-                    add_term_meta($term_id, 'taxonomy-banner-image-id', sanitize_text_field($banner_slider), false);
-                }
+        // Security checks
+        if (!isset($_POST['taxonomy-banner-image-id']) || 
+            !current_user_can('edit_term', $term_id) ||
+            !wp_verify_nonce($_POST['_wpnonce'], "update-tag_{$term_id}")) {
+            return;
+        }
+        
+        // Process banner images
+        $banner_images = array();
+        
+        foreach ($_POST['taxonomy-banner-image-id'] as $banner_img_id) {
+            $clean_id = sanitize_text_field($banner_img_id);
+            if (!empty($clean_id) && is_numeric($clean_id)) {
+                $banner_images[] = $clean_id;
             }
+        }
+        
+        // Remove empty values and update meta
+        $banner_images = array_filter($banner_images);
+        
+        if (!empty($banner_images)) {
+            update_term_meta($term_id, 'taxonomy-banner-image-id', $banner_images);
+        } else {
+            delete_term_meta($term_id, 'taxonomy-banner-image-id');
         }
     }
 
     public function get_all_taxnomy_banner_slug(){
-        $data = get_option( 'get_taxnomies_slug' );
-        $data = str_replace( ' ', '' , $data);;
+        $data = get_option( 'get_taxnomies_slug', 'category | post_tag' );        
+        $data = str_replace( ' ', '', $data );
         $data = explode( '|', $data );
         return $data;
     }
--- includes/Views/Admin/edit_taxonomy_banner_image.php
+++ includes/Views/Admin/edit_taxonomy_banner_image.php
@@ -1,17 +1,44 @@
-<div class="form-field term-banner-thumbnail-wrap">
-    <label for="dwtbs_banner_slider"><?php esc_html_e( 'Banner Images', 'taxonomy-banner-slider' ); ?></label>
-    <div class="banner-slider-container">
-        <div class="banner-slider-item">
-            <div class="product_cat_thumbnail" style="float: left; margin-right: 10px;">
-                <img src="<?php echo esc_url(wc_placeholder_img_src()); ?>" width="60px" height="60px">
-            </div>
-            <div>
-                <input type="hidden" class="image_attachment_id" name="dwtbs_banner_slider[]">
-                <button type="button" class="button upload_image_button"><?php esc_html_e('Upload/Add image', 'taxonomy-banner-slider'); ?></button>
-                <button type="button" class="button remove_image_button" style="display: none;"><?php esc_html_e('Remove image', 'taxonomy-banner-slider'); ?></button>
-            </div>
+<?php var_dump($this->banner_sliders);?>
+<tr class="form-field term-banner-thumbnail-wrap">
+    <th scope="row" valign="top"><label><?php esc_html_e('Banner Images', 'advanced-taxonomy-image-banner'); ?></label></th>
+    <td>
+        <div class="banner-slider-container" id="atbi-banner-container">
+            <?php foreach ($this->banner_sliders as $index => $banner_slider): ?>
+                <div class="banner-slider-item">
+                    <div class="taxonomy-banner-image-preview-wrapper" style="float: left; margin-right: 10px;">
+                        <div class="taxonomy-banner-image-preview">
+                            <img src="<?php echo $banner_slider ? wp_get_attachment_image_url($banner_slider, 'thumbnail') : ''; ?>" style="<?php echo empty($banner_slider) ? 'display: none;' : ''; ?>">
+                        </div>
+                    </div>
+
+                    <div class="taxonomy-banner-image-actions">
+                        <input type="hidden" class="image_attachment_banner_id" name="taxonomy-banner-image-id[]" value="<?php echo esc_attr($banner_slider); ?>">
+                        
+                        <button type="button" class="button button-secondary taxonomy_media_banner_button components-button is-secondary">
+                            <span class="dashicons dashicons-format-image"></span>
+                            <?php _e('Add Image', 'advanced-taxonomy-image-banner'); ?>
+                        </button>
+                        
+                        <button type="button" class="button button-secondary taxonomy_media_banner_remove components-button is-destructive" <?php echo empty($banner_slider) ? 'style="display: none;"' : ''; ?>>
+                            <span class="dashicons dashicons-trash"></span>
+                            <?php _e('Remove Image', 'advanced-taxonomy-image-banner'); ?>
+                        </button>
+                        
+                        <?php if ($index > 0): ?>
+                            <button type="button" class="button button_remove_banner_button">
+                                <span class="dashicons dashicons-trash"></span>
+                                <?php _e('Remove Banner', 'advanced-taxonomy-image-banner'); ?>
+                            </button>
+                        <?php endif; ?>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            <?php endforeach; ?>
         </div>
-    </div>
-    <div class="clear" style="margin-top:10px;"></div>
-    <button type="button" class="button add_new_banner_button"><span class="dashicons dashicons-plus"></span><?php esc_html_e('Add New Banner', 'taxonomy-banner-slider'); ?></button>
-</div>
+        
+        <button type="button" class="button add_new_banner_button">
+            <span class="dashicons dashicons-plus"></span>
+            <?php esc_html_e('Add New Banner', 'advanced-taxonomy-image-banner'); ?>
+        </button>
+    </td>
+</tr>
