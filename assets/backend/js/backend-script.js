9757a43b61cd8c07289272911d72b7611387f4b5
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2025-08-21 16:54:51 +0530

    Fix script issue

--- assets/backend/js/backend-script.js
+++ assets/backend/js/backend-script.js
@@ -49,16 +49,20 @@ jQuery(document).ready(function($) {
 
 /* Banner images */
 jQuery(document).ready(function($) {
-   // Add new banner
+    // Add new banner
     $(document).on('click', '.add_new_banner_button', function() {
         var $container = $('.banner-slider-container');
         var $firstItem = $container.find('.banner-slider-item').first();
         
         if ($firstItem.length) {
             var $clone = $firstItem.clone();
+            
+            // Reset values for the clone
             $clone.find('.image_attachment_banner_id').val('');
-            $clone.find('.button_remove_image_button').hide();
-            $clone.find('.button_remove_banner_button').remove();
+            $clone.find('.taxonomy-banner-image-preview').html('');
+            $clone.find('.taxonomy_media_banner_remove').hide();
+            
+            // Add remove banner button
             $clone.append('<button type="button" class="button button_remove_banner_button"><span class="dashicons dashicons-trash"></span></button>');
             $container.append($clone);
         }
@@ -73,8 +77,10 @@ jQuery(document).ready(function($) {
     $(document).on('click', '.taxonomy_media_banner_button', function(e) {
         e.preventDefault();
         var $button = $(this);
-        var $input = $button.siblings('.image_attachment_banner_id');
-        var $img = $button.closest('.banner-slider-item').find('img');
+        var $item = $button.closest('.banner-slider-item');
+        var $input = $item.find('.image_attachment_banner_id');
+        var $preview = $item.find('.taxonomy-banner-image-preview');
+        var $removeBtn = $item.find('.taxonomy_media_banner_remove');
 
         // Create media frame
         var frame = wp.media({
@@ -85,13 +91,11 @@ jQuery(document).ready(function($) {
 
         frame.on('select', function() {
             var attachment = frame.state().get('selection').first().toJSON();
-            console.log($input);
             $input.val(attachment.id);
-            $('.taxonomy-image-preview').html('<img src="' + attachment.url + '" alt="" style="max-width:100%;"/>');
-            // Add class to indicate image is present
-            $('.taxonomy-image-preview').addClass('has-image');
-            $img.attr('src', attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url);
-            $button.siblings('.taxonomy_media_banner_remove').show();
+            $preview.html('<img src="' + attachment.url + '" alt="" style="max-width:100%;"/>');
+            
+            // Show the remove button
+            $removeBtn.show();
         });
 
         frame.open();
@@ -101,7 +105,10 @@ jQuery(document).ready(function($) {
     $(document).on('click', '.taxonomy_media_banner_remove', function(e) {
         e.preventDefault();
         var $button = $(this);
-        $button.siblings('.image_attachment_id').val('');
+        var $item = $button.closest('.banner-slider-item');
+        
+        $item.find('.image_attachment_banner_id').val('');
+        $item.find('.taxonomy-banner-image-preview').html('');
         $button.hide();
     });
 });
